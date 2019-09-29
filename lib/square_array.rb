8a1a3bc5af3438e89858a0d1ccc2c8def220128e
kelly r maass <kmaass77627@gmail.com>
2019-09-29 22:13:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,6 +5,16 @@
 #end
 
 #my_arr = [1, 2]
-p square_array(my_arr)
+#p square_array(my_arr)
+
+def square_array(numbers)
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
 
 
