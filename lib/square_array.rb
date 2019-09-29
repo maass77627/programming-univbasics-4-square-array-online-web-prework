fe1f6952c3c397a5e656ad0e33777d41611c635d
kelly r maass <kmaass77627@gmail.com>
2019-09-29 22:12:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
- arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
+#def square_array(array)
+# arr = []
+ # array.each { |i| arr << i ** 2 }
+ # arr
 end
 
 my_arr = [1, 2]
